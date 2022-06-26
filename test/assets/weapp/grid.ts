6e5033b643382ebe88830b0a0a917b9f46ec6397
MellowCo <799478052@qq.com>
2022-06-26 16:29:23 +0800

    feat: grid

---
+++ test/assets/weapp/grid.ts
@@ -0,0 +1,26 @@
+/*
+ * @Author: licl
+ * @Date: 2022-06-26 16:27:20
+ * @LastEditTime: 2022-06-26 16:27:21
+ * @LastEditors: licl
+ * @Description:
+ */
+export const grid = [
+  'grid-cols-2',
+  'grid-rows-3',
+  'grid',
+  'auto-rows-min',
+  'auto-rows-fr',
+  'row-auto',
+  'auto-cols-auto',
+  'auto-rows-auto',
+  'col-span-1',
+  'row-span-full',
+  'row-end-1',
+  'row-start-full',
+  'auto-flow-col-dense',
+  'grid-cols-minmax-1rem',
+  'grid-rows-minmax-100px',
+  'grid-flow-dense',
+  'grid-flow-col-dense',
+]
--- test/preset-weapp.test.ts
+++ test/preset-weapp.test.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: licl
  * @Date: 2022-06-25 13:30:37
- * @LastEditTime: 2022-06-26 16:19:13
+ * @LastEditTime: 2022-06-26 16:28:37
  * @LastEditors: licl
  * @Description:
  */
@@ -9,7 +9,7 @@ import { createGenerator } from '@unocss/core'
 import { describe, expect, test } from 'vitest'
 import presetMini from '../src/index'
 import { presetMiniTargets } from './assets/preset-mini-targets'
-import { align, bg, border, borderColor, color, flex, shadow, size, spacing, typography } from './assets/weapp'
+import { align, bg, border, borderColor, color, flex, grid, shadow, size, spacing, typography } from './assets/weapp'
 
 const uno = createGenerator({
   presets: [
@@ -84,6 +84,12 @@ describe('preset-weapp', () => {
 
   test('flex', async () => {
     const code = flex.join(' ')
+    const { css } = await uno.generate(code)
+    expect(css).toMatchSnapshot()
+  })
+
+  test('grid', async () => {
+    const code = grid.join(' ')
     const { css } = await uno.generate(code)
     expect(css).toMatchSnapshot()
   })
