e0891f681799cc3853e50588d0a69d87ff111e51
MellowCo <799478052@qq.com>
2022-08-24 20:16:31 +0800

    chore: rename function

--- src/rpxTranform/taro.ts
+++ src/rpxTranform/taro.ts
@@ -1,14 +1,19 @@
-// taro 匹配 rpx 和 px 结尾
-export const taroRpxRE = /^-?[\.\d]+r?px$/
-
 // taro/packages/taro-api/src/tool.js
-export function taroWechartRpxTransform(size: any, designWidth: any, deviceRatio: any) {
+// px -> rpx
+export function taroPxToRpx(size: any, designWidth: any, deviceRatio: any) {
   if (!(designWidth in deviceRatio))
     throw new Error(`deviceRatio 配置中不存在 ${designWidth} 的设置！`)
   return `${parseInt(size, 10) * deviceRatio[designWidth]}rpx`
 }
 
+// rpx -> px
+export function taroRpxToPx(size: any, designWidth: any, deviceRatio: any) {
+  if (!(designWidth in deviceRatio))
+    throw new Error(`deviceRatio 配置中不存在 ${designWidth} 的设置！`)
+  return parseInt(size, 10) / deviceRatio[designWidth]
+}
+
 // taro/packages/taro-h5/src/api/taro.ts
-export function taroH5RpxTransform(size: any, designWidth: any) {
+export function taroPxToRem(size: any, designWidth: any) {
   return `${Math.ceil((((parseInt(size, 10) / 40) * 640) / designWidth) * 10000) / 10000}rem`
 }
--- src/rpxTranform/uniapp.ts
+++ src/rpxTranform/uniapp.ts
@@ -1,6 +1,3 @@
-// uniapp 只匹配 rpx 结尾
-export const uniappRpxRE = /^-?[\.\d]+rpx$/
-
 /**
  * uniapp postcss rpx 转换规则
  * pkg: @dcloudio/vue-cli-plugin-uni/packages/postcss 37行
