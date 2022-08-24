7890ba2048e6f8aeb596d69310b642f3c66fd8f9
MellowCo <799478052@qq.com>
2022-08-24 20:17:54 +0800

    test: rpx test

---
+++ test/rpx.test.ts
@@ -0,0 +1,96 @@
+import { createGenerator } from '@unocss/core'
+import { describe, expect, test } from 'vitest'
+import presetMini from '../src'
+
+const taroH5_750 = createGenerator({
+  presets: [
+    presetMini({
+      isH5: true,
+      platform: 'taro',
+    }),
+  ],
+})
+
+const taroWe_750 = createGenerator({
+  presets: [
+    presetMini({
+      isH5: false,
+      platform: 'taro',
+    }),
+  ],
+})
+
+const taroH5_375 = createGenerator({
+  presets: [
+    presetMini({
+      isH5: true,
+      platform: 'taro',
+      designWidth: 375,
+      deviceRatio: {
+        375: 2 / 1,
+      },
+    }),
+  ],
+})
+
+const taroWe_375 = createGenerator({
+  presets: [
+    presetMini({
+      isH5: false,
+      platform: 'taro',
+      designWidth: 375,
+      deviceRatio: {
+        375: 2 / 1,
+      },
+    }),
+  ],
+})
+
+const uniapp_750 = createGenerator({
+  presets: [
+    presetMini({
+      isH5: true,
+      platform: 'uniapp',
+    }),
+  ],
+})
+
+const size = [
+  'w-100px',
+  'w-100',
+  'w-100rpx',
+  'text-base',
+  'm-6',
+]
+
+describe('rpx-tranform', () => {
+  test('taroH5_750', async () => {
+    const code = size.join(' ')
+    const { css } = await taroH5_750.generate(code)
+    expect(css).toMatchSnapshot()
+  })
+
+  test('taroWe_750', async () => {
+    const code = size.join(' ')
+    const { css } = await taroWe_750.generate(code)
+    expect(css).toMatchSnapshot()
+  })
+
+  test('taroH5_375', async () => {
+    const code = size.join(' ')
+    const { css } = await taroH5_375.generate(code)
+    expect(css).toMatchSnapshot()
+  })
+
+  test('taroWe_375', async () => {
+    const code = size.join(' ')
+    const { css } = await taroWe_375.generate(code)
+    expect(css).toMatchSnapshot()
+  })
+
+  test('uniapp_750', async () => {
+    const code = size.join(' ')
+    const { css } = await uniapp_750.generate(code)
+    expect(css).toMatchSnapshot()
+  })
+})
