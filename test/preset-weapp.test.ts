e9c574c604f86ef1e967edf3f5d3958711804449
MellowCo <799478052@qq.com>
2022-09-10 16:03:22 +0800

    fix: fix escaped underscore replacements in h.bracket

--- test/preset-weapp.test.ts
+++ test/preset-weapp.test.ts
@@ -1,7 +1,7 @@
 import { createGenerator } from '@unocss/core'
 import { describe, expect, test } from 'vitest'
 import presetWeapp from '../src/index'
-import { align, animation, background, bg, border, borderColor, color, flex, grid, position, replaceAll, safeArea, shadow, size, spacing, typography } from './assets/weapp'
+import { presetMiniTargets } from './assets/preset-mini-targets'
 
 const uno = createGenerator({
   presets: [
@@ -20,98 +20,8 @@ const uno = createGenerator({
 })
 
 describe('preset-weapp', () => {
-  test('size', async () => {
-    const code = size.join(' ')
-    const { css } = await uno.generate(code)
-    expect(css).toMatchSnapshot()
-  })
-
-  test('border', async () => {
-    const code = border.join(' ')
-    const { css } = await uno.generate(code)
-    expect(css).toMatchSnapshot()
-  })
-
-  test('borderColor', async () => {
-    const code = borderColor.join(' ')
-    const { css } = await uno.generate(code)
-    expect(css).toMatchSnapshot()
-  })
-
-  test('align', async () => {
-    const code = align.join(' ')
-    const { css } = await uno.generate(code)
-    expect(css).toMatchSnapshot()
-  })
-
-  test('color', async () => {
-    const code = color.join(' ')
-    const { css } = await uno.generate(code)
-    expect(css).toMatchSnapshot()
-  })
-
-  test('bg', async () => {
-    const code = bg.join(' ')
-    const { css } = await uno.generate(code)
-    expect(css).toMatchSnapshot()
-  })
-
-  test('typography', async () => {
-    const code = typography.join(' ')
-    const { css } = await uno.generate(code)
-    expect(css).toMatchSnapshot()
-  })
-
-  test('spacing', async () => {
-    const code = spacing.join(' ')
-    const { css } = await uno.generate(code)
-    expect(css).toMatchSnapshot()
-  })
-
-  test('shadow', async () => {
-    const code = shadow.join(' ')
-    const { css } = await uno.generate(code)
-    expect(css).toMatchSnapshot()
-  })
-
-  test('flex', async () => {
-    const code = flex.join(' ')
-    const { css } = await uno.generate(code)
-    expect(css).toMatchSnapshot()
-  })
-
-  test('grid', async () => {
-    const code = grid.join(' ')
-    const { css } = await uno.generate(code)
-    expect(css).toMatchSnapshot()
-  })
-
-  test('position', async () => {
-    const code = position.join(' ')
-    const { css } = await uno.generate(code)
-    expect(css).toMatchSnapshot()
-  })
-
-  test('replaceAll', async () => {
-    const code = replaceAll.join(' ')
-    const { css } = await uno.generate(code)
-    expect(css).toMatchSnapshot()
-  })
-
-  test('safeArea', async () => {
-    const code = safeArea.join(' ')
-    const { css } = await uno.generate(code)
-    expect(css).toMatchSnapshot()
-  })
-
-  test('animation', async () => {
-    const code = animation.join(' ')
-    const { css } = await uno.generate(code)
-    expect(css).toMatchSnapshot()
-  })
-
-  test('background', async () => {
-    const code = background.join(' ')
+  test('presetMiniTargets', async () => {
+    const code = presetMiniTargets.join(' ')
     const { css } = await uno.generate(code)
     expect(css).toMatchSnapshot()
   })
