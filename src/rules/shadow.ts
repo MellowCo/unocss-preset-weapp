48fda83049a8670dec86c04cece04288b3aada39
MellowCo <799478052@qq.com>
2023-04-08 09:38:22 +0800

    feat: support custom shadow color

--- src/rules/shadow.ts
+++ src/rules/shadow.ts
@@ -1,7 +1,7 @@
 import type { Rule } from '@unocss/core'
 import { restoreSelector } from 'unplugin-transform-class/utils'
 import type { Theme } from '../theme'
-import { colorResolver, colorableShadows, handler as h } from '../utils'
+import { colorResolver, colorableShadows, handler as h, hasParseableColor } from '../utils'
 import { varEmpty } from './static'
 
 export const boxShadowsBase = {
@@ -16,11 +16,13 @@ export const boxShadows: Rule<Theme>[] = [
     let [, d] = match
     const { theme } = context
     d = restoreSelector(d, theme.transformRules)
-    const v = theme.boxShadow?.[d || 'DEFAULT'] || h.bracket.cssvar(d)
 
-    if (v) {
+    const v = theme.boxShadow?.[d || 'DEFAULT']
+    const c = d ? h.bracket.cssvar(d) : undefined
+
+    if ((v != null || c != null) && !hasParseableColor(c, theme)) {
       return {
-        '--un-shadow': colorableShadows(v, '--un-shadow-color').join(','),
+        '--un-shadow': colorableShadows((v || c)!, '--un-shadow-color').join(','),
         'box-shadow': 'var(--un-ring-offset-shadow), var(--un-ring-shadow), var(--un-shadow)',
       }
     }
