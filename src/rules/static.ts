452ec8806a302e8889c052f2b48872bdd44689db
MellowCo <799478052@qq.com>
2023-09-24 11:29:12 +0800

    feat: remove font-smoothing from antialiased rules

--- src/rules/static.ts
+++ src/rules/static.ts
@@ -144,12 +144,10 @@ export const fontSmoothings: Rule[] = [
   ['antialiased', {
     '-webkit-font-smoothing': 'antialiased',
     '-moz-osx-font-smoothing': 'grayscale',
-    'font-smoothing': 'grayscale',
   }],
   ['subpixel-antialiased', {
     '-webkit-font-smoothing': 'auto',
     '-moz-osx-font-smoothing': 'auto',
-    'font-smoothing': 'auto',
   }],
 ]
 
