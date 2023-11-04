833246041d32f6ec5c1ed10a79d59f44bd6575ba
MellowCo <799478052@qq.com>
2023-11-04 09:20:17 +0800

    chore: build

--- build.config.ts
+++ build.config.ts
@@ -1,4 +1,5 @@
 import { defineBuildConfig } from 'unbuild'
+import { isWindows } from 'std-env'
 
 export default defineBuildConfig({
   entries: [
@@ -14,5 +15,9 @@ export default defineBuildConfig({
   declaration: true,
   rollup: {
     emitCJS: true,
+    dts: {
+      respectExternal: false,
+    },
   },
+  failOnWarn: !isWindows,
 })
