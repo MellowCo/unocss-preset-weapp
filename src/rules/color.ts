f0b00199c51e0ae752c3d7c60cf360e78ff13c70
MellowCo <799478052@qq.com>
2024-02-24 09:30:30 +0800

    feat: background color autocomplete

--- src/rules/color.ts
+++ src/rules/color.ts
@@ -28,7 +28,7 @@ export const bgColors: Rule<Theme>[] = [
     if ((isSize(d) || bgPositionRE.test(d)) && h.bracketOfPosition(d) != null)
       return { 'background-position': h.bracketOfPosition(d)!.split(' ').map(e => h.position.fraction.auto.px.cssvar(e) ?? e).join(' ') }
     return colorResolver('background-color', 'bg', 'backgroundColor')(params, body)
-  }],
+  }, { autocomplete: 'bg-$colors' }],
   [/^bg-op(?:acity)?-?(.+)$/, ([, opacity]) => ({ '--un-bg-opacity': h.bracket.percent.cssvar(opacity) }), { autocomplete: 'bg-(op|opacity)-<percent>' }],
 ]
 
