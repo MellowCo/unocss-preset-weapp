3bfd36b72bd8be0d67c2ccf3c0a2b9c2f9c9e01e
licl <licl@>
2024-05-05 09:00:14 +0800

    fix: remove LF in question mark rule

--- src/rules/question-mark.ts
+++ src/rules/question-mark.ts
@@ -3,10 +3,6 @@ import type { Rule } from '@unocss/core'
 /**
  * Used for debugging, only avaliable in development mode.
  *
- * @param _
- * @param root0
- * @param root0.constructCSS
- * @param root0.generator
  * @example `?` / `where`
  */
 export const questionMark: Rule[] = [
@@ -14,7 +10,7 @@ export const questionMark: Rule[] = [
     /^(where|\?)$/,
     (_, { constructCSS, generator }) => {
       if (generator.userConfig.envMode === 'dev')
-        return `@keyframes __un_qm{0%{box-shadow:inset 4px 4px #ff1e90, inset -4px -4px #ff1e90}100%{box-shadow:inset 8px 8px #3399ff, inset -8px -8px #3399ff}}\n${constructCSS({ animation: '__un_qm 0.5s ease-in-out alternate infinite' })}`
+        return `@keyframes __un_qm{0%{box-shadow:inset 4px 4px #ff1e90, inset -4px -4px #ff1e90}100%{box-shadow:inset 8px 8px #3399ff, inset -8px -8px #3399ff}} ${constructCSS({ animation: '__un_qm 0.5s ease-in-out alternate infinite' })}`
     },
   ],
 ]
